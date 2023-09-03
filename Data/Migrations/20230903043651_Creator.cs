a18f7346c5ecd4580fcfb0476826810b0baee4b4
trev191 <trev191@yahoo.com>
2023-09-02 21:55:47 -0700

    Update database with Creator field. Add properties to member variables (Display Name, Required, ..)

---
+++ Data/Migrations/20230903043651_Creator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokesWebApp.Data.Migrations
+{
+    public partial class Creator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Creator",
+                table: "Joke",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Creator",
+                table: "Joke");
+        }
+    }
+}
--- Models/Joke.cs
+++ Models/Joke.cs
@@ -1,11 +1,26 @@
-﻿namespace JokesWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JokesWebApp.Models
 {
     public class Joke
     {
         public int Id { get; set; }
+
+        [Display(Name = "Joke Question")]
+        [DataType(DataType.Text)]
+        [Required]
         public string JokeQuestion { get; set; }
+
+        [Display(Name = "Joke Answer")]
+        [DataType(DataType.Text)]
+        [Required]
         public string JokeAnswer { get; set; }
+
+        [Display(Name = "Creator")]
+        [DataType(DataType.Text)]
         public string Creator { get; set; }
+
         public Joke()
         {
             
